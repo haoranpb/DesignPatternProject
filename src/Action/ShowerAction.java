2c0014a7d59ea4337d291b26b9f121e2826b44aa
Ywh917670214 <917670214@qq.com>
2018-10-29 15:38:22 +0800

    Add files via upload

--- src/Action/ShowerAction.java
+++ src/Action/ShowerAction.java
@@ -4,11 +4,13 @@ package Action;
 import Base.Animal;
 import Base.AnimalAction;
 
-public abstract class ShowerAction implements AnimalAction {
+public class ShowerAction implements AnimalAction {
 	Animal self;
 	private ShowerAction nextHandler;
+	public int mode;
 	@Override
 	public void getself(Animal getanimal) {
+		// TODO Auto-generated method stub
 		self = getanimal;
 	}
 
@@ -20,7 +22,13 @@ public abstract class ShowerAction implements AnimalAction {
 	public void setNext(ShowerAction handler){
         this.nextHandler = handler;
     }
-	
-
-	
+	public void doshower() {
+		if (mode == 1)
+			System.out.println(self.Animalname + " do shower in sand");
+		else if (mode == 2)
+			System.out.println(self.Animalname + " do shower in water");
+		else
+			System.out.println(self.Animalname + " don't need to do shower");
+	}
+		
 }
