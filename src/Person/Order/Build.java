a1c9bf31f91cd83f58f141cec122c2144a5a1fe6
ynxia <ynxia1@gmail.com>
2018-10-30 23:20:38 +0800

    update Build.java

---
+++ src/Person/Order/Build.java
@@ -0,0 +1,35 @@
+package Person.Order;
+
+import Item.BuildingValue.BuildingType;
+import Item.BuildingValue.Strategy.SimpleBuilding.SimpleBuildStrat;
+import Item.BuildingValue.Strategy.ExquisiteBuilding.ExquisiteBuildStrat;
+import Item.BuildingValue.Strategy.LuxuryBuilding.LuxuryBuildStrat;
+import Item.BuildingValue.Strategy.BuildStrategy;
+import Item.BuildingValue.BuildingForPeople;
+
+/**
+ * Using Strategy
+ * @author XYN
+ * @version 2018-10-30 01:30
+ */
+public class Build {
+    private BuildStrategy bdstr;
+
+    public Build() {
+    }
+
+    protected void setBuildStrat(BuildingType buildingType) {
+        if (buildingType == BuildingType.SIMP) {
+            bdstr = new SimpleBuildStrat();
+        } else if (buildingType == BuildingType.EXQU) {
+            bdstr = new ExquisiteBuildStrat();
+        } else if (buildingType == BuildingType.LUXU) {
+            bdstr = new LuxuryBuildStrat();
+        }
+    }
+    public BuildingForPeople doBuilding(BuildingType buildingType) {
+        setBuildStrat(buildingType);
+        return bdstr.building();
+    };
+
+}
